d20454f91e4384005122c9ffac8dc27456003c2d
Armita <armitashahim9@gmail.com>
2026-08-29 11:39:24 +0330

    chore: set PDO fetch mode

--- src/core/Database.php
+++ src/core/Database.php
@@ -25,6 +25,7 @@ class Database
             $dsn = 'pgsql:host=' . $this->host . ';port=' . $this->port . ';dbname=' . $this->dbname;
             $pdo = new PDO($dsn, $this->username, $this->password);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             return $pdo;
         } catch (PDOException $e) {
             echo 'database connection failed:' . $e->getMessage();
