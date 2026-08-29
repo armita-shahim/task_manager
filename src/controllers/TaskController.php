0b3c2f7f276a917cdcab28a36687ba752d642ec6
Armita <armitashahim9@gmail.com>
2026-08-29 21:54:09 +0330

    feat: add task controller

---
+++ src/controllers/TaskController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\controllers;
+
+use App\models\Task;
+use App\enums\Priority;
+use App\enums\Status;
+use DateTime;
+
+class TaskController
+{
+
+    public function store(): void
+    {
+        $json = file_get_contents('php://input');
+        $input = json_decode($json, true);
+
+        $priority = Priority::from($input['priority']);
+        $dueDate = new DateTime($input['due_date']);
+        $status = Status::from($input['status']);
+
+        Task::addTask(
+            $input['title'],
+            $input['description'],
+            $priority,
+            $dueDate,
+            $status,
+            $input['category_id']
+        );
+
+        header('Content-Type: application/json');
+        echo json_encode(
+            ['message' => 'Task created successfully'],
+            JSON_PRETTY_PRINT
+        );
+    }
+
+    public function index(): void
+    {
+        $tasks = Task::allTasks();
+        $json = json_encode($tasks, JSON_PRETTY_PRINT);
+        header('Content-Type: application/json');
+        echo $json;
+    }
+
+    public function show(int $id): void
+    {
+        $task = Task::findTask($id);
+        if ($task !== null) {
+            $json = json_encode($task, JSON_PRETTY_PRINT);
+            header('Content-Type: application/json');
+            echo $json;
+        } else {
+            header('Content-Type: application/json');
+            echo json_encode(
+                ['message' => 'task with this id not found'],
+                JSON_PRETTY_PRINT
+            );
+        }
+    }
+
+    public function update(int $id): void
+    {
+        $json = file_get_contents('php://input');
+        $input = json_decode($json, true);
+
+        $priority = Priority::from($input['priority']);
+        $dueDate = new DateTime($input['due_date']);
+        $status = Status::from($input['status']);
+
+        Task::updateTask(
+            $id,
+            $input['title'],
+            $input['description'],
+            $priority,
+            $dueDate,
+            $status,
+            $input['category_id']
+        );
+
+        header('Content-Type: application/json');
+        echo json_encode(
+            ['message' => 'Task updated successfully'],
+            JSON_PRETTY_PRINT
+        );
+    }
+
+    public function destroy(int $id): void
+    {
+        Task::deleteTask($id);
+        header('Content-Type: application/json');
+        echo json_encode(
+            ['message' => 'task with this id deleted successfully'],
+            JSON_PRETTY_PRINT
+        );
+    }
+}
