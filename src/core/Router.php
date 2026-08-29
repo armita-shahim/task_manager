4a62bf1f9d355cc824a3c689119f1ee2f509d8e5
Armita <armitashahim9@gmail.com>
2026-08-29 21:48:54 +0330

    feat: add dynamic route parameter handling

--- src/core/Router.php
+++ src/core/Router.php
@@ -11,43 +11,79 @@ class Router
         [
             'method' => 'POST',
             'uri' => '/tasks',
-            'action' => [TaskController::class, 'create']
+            'action' => [TaskController::class, 'store']
         ],
 
         [
             'method' => 'GET',
             'uri' => '/tasks',
-            'action' => [TaskController::class, 'list']
+            'action' => [TaskController::class, 'index']
         ],
 
         [
             'method' => 'PUT',
-            'uri' => '/tasks',
+            'uri' => '/tasks/{id}',
             'action' => [TaskController::class, 'update']
         ],
 
         [
             'method' => 'DELETE',
-            'uri' => '/tasks',
-            'action' => [TaskController::class, 'delete']
+            'uri' => '/tasks/{id}',
+            'action' => [TaskController::class, 'destroy']
+        ],
+
+        [
+            'method' => 'GET',
+            'uri' => '/tasks/{id}',
+            'action' => [TaskController::class, 'show']
         ]
     ];
 
 
     public function resolve(): void
     {
-        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $result = parse_url($_SERVER['REQUEST_URI']);
+        $uri = $result['path'];
 
         $method = $_SERVER['REQUEST_METHOD'];
 
         for ($i = 0; $i < count($this->routes); $i++) {
             $route = $this->routes[$i];
-            if ($route['method'] === $method && $route['uri'] === $uri) {
+
+            if ($route['method'] !== $method) {
+                continue;
+            }
+
+            $routeParts = explode('/', $route['uri']);
+            $uriParts = explode('/', $uri);
+
+            if (count($routeParts) !== count($uriParts)) {
+                continue;
+            }
+
+            $id = null;
+            $match = true;
+            for ($j = 0; $j < count($routeParts); $j++) {
+
+                if ($routeParts[$j] === '{id}') {
+                    $id = $uriParts[$j];
+                } elseif ($routeParts[$j] !== $uriParts[$j]) {
+                    $match = false;
+                    break;
+                }
+            }
+
+            if ($match) {
                 $controllerClass = $route['action'][0];
                 $actionMethod = $route['action'][1];
 
                 $controller = new $controllerClass;
-                $controller->$actionMethod();
+                if ($id !== null) {
+                    $controller->$actionMethod((int) $id);
+                } else {
+                    $controller->$actionMethod();
+                }
+
                 return;
             }
         }
