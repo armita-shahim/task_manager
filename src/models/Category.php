2d8a5c91140acc4ed938e880dd09a8f1acb698fe
Armita <armitashahim9@gmail.com>
2026-08-30 15:51:46 +0330

    feat: add category model

---
+++ src/models/Category.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\models;
+
+use App\core\Database;
+
+class Category
+{
+
+    public static function allCategories(): array
+    {
+        $database = new Database();
+        $pdo = $database->connect();
+
+        $sql = 'SELECT * FROM categories';
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+
+    public static function findCategory(int $id): ?array
+    {
+        $database = new Database();
+        $pdo = $database->connect();
+
+        $sql = 'SELECT * FROM categories WHERE id = :id';
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(['id' => $id]);
+        $result = $stmt->fetch();
+        if ($result === false) {
+            return null;
+        } else {
+            return $result;
+        }
+    }
+
+    public static function addCategory(string $name): void
+    {
+        $database = new Database();
+        $pdo = $database->connect();
+
+        $sql = "INSERT INTO categories (name) VALUES (:name)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(['name' => $name]);
+    }
+
+    public static function updateCategory(int $id, string $name): void
+    {
+        $database = new Database();
+        $pdo = $database->connect();
+
+        $sql = "UPDATE categories SET name = :name WHERE id = :id";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(['id' => $id, 'name' => $name]);
+    }
+
+    public static function deleteCategory(int $id): void
+    {
+        $database = new Database();
+        $pdo = $database->connect();
+
+        $sql = 'DELETE FROM categories WHERE id = :id';
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(['id' => $id]);
+    }
+}
